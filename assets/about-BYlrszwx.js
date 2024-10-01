565c8e55a85bde5bf895aa02b38410c3ae35fc1a
sarum90 <sarum90@users.noreply.github.com>
2024-10-01 21:03:22 +0000

    deploy: 7929c13b638c97e2dfdae40757f5c5fa80b4a39c

--- assets/about-BYlrszwx.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,r,A as o}from"./App-CLIAgs6S.js";e(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
---
+++ assets/about-DGMGUM9F.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,r,A as o}from"./App-k_PZjLCX.js";e(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
